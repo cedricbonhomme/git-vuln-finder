0a10082135f9d95fdd629d3a91b4beacea9a55b1
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-01-09 09:11:05 +0100

    Added teardown function after all tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,29 @@
 
 import os
+import shutil
 import pytest
 
 from git import Repo
 
+TESTS_FOLDER = './test_repos'
 
 @pytest.fixture(scope='session')
-def clone_curl():
-    """Clone the repository of curl for the tests."""
+def clone_curl(request):
+    """Clone the repository of curl for the tests.
+    """
     git_url = 'https://github.com/curl/curl.git'
-    repo_dir = './test_repos/curl'
+    repo_dir = os.path.join(TESTS_FOLDER, 'curl')
     repo = Repo.clone_from(url=git_url, to_path=repo_dir)
     #repo.heads['curl-7_67_0'].checkout()
 
     def teardown():
-        os.unlink(repo_dir)
+        shutil.rmtree(repo_dir)
+
+    request.addfinalizer(teardown)
 
     return repo_dir
+
+def pytest_unconfigure(config):
+    """Teardown after all tests.
+    """
+    shutil.rmtree(TESTS_FOLDER)
